ac8ad6d6c98191b865948e46ae0d94e40fe81615
vita <954565531@qq.com>
2020-10-13 16:46:44 +0800

    Common工程新建

---
+++ gkd-shell-common/src/main/java/com/asiainfo/gkdshellcommon/GkdShellCommonApplication.java
@@ -0,0 +1,13 @@
+package com.asiainfo.gkdshellcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkdShellCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkdShellCommonApplication.class, args);
+	}
+
+}
---
+++ gkd-shell-common/src/test/java/com/asiainfo/gkdshellcommon/GkdShellCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiainfo.gkdshellcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkdShellCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
